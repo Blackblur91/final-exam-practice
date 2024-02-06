d11e0b2fc57756eaad58cf984a7f3f916d55609a
Blackblur91 <feketebalazs04@gmail.com>
2024-02-06 20:30:09 +0100

    sort by finished

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react"
 import Person from "./Person"
 
-function People( {people}) {
+function People({ people }) {
   const [searchString, setSearchString] = useState("")
+  const [peopleData, setPeopleData] = useState(people)
+  const [sortBy, setSortBy] = useState ("asc")
+
+
+  const handleSort = () => {
+    setPeopleData([...people]
+      .sort((a, b) => sortBy === "asc"
+      ? a.name.localeCompare(b.name) 
+      : b.name.localeCompare(a.name)
+      ))
+      setSortBy(sortBy === "asc" ? "desc" : "asc")
+    }
+
   return (
     <>
       <input type="text" onChange={e => setSearchString(e.target.value)}/>
-      {people
+
+    <button onClick={handleSort}>sort by {sortBy}</button>
+
+      {peopleData
         .filter(person => person.name.toLowerCase().includes(searchString.toLowerCase()))
         .map((person, index) => <Person key = {index} person = {person}/>)}
-
-
     </>
     )
 }
